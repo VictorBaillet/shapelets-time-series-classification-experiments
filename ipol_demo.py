812d25b0d78a277a578242ee481d788ef5131cae
Spip974 <victorbaillet2000@gmail.com>
2024-03-11 23:19:44 +0100

    add plots in ipol

--- ipol_demo.py
+++ ipol_demo.py
@@ -17,6 +17,19 @@ from utils.general_utils import generate_synthetic_dataset
 def main(args):
     x_train, y_train, x_test, y_test = generate_synthetic_dataset(args.noise)
 
+    fig2, axes2 = plt.subplots(nrows=1, ncols=2, figsize=(15, 5))
+    
+    # Plot the first time series from X_train
+    axes2[0].plot(x_train[0])
+    axes2[0].set_title("First class time series example")
+    
+    # Plot the last time series from X_train
+    axes2[1].plot(x_train[-1])
+    axes2[1].set_title("Second class time series example")
+
+    # Save the new figure to a different file
+    plt.savefig("time_series_examples.png")
+
     x_shapelet = shapelet_cached_selection(x_train, y_train, args.shapelet_lenght_min, 
                                            args.shapelet_lenght_max, args.n_shapelet, compute_f_stat, verbose=1)
 
